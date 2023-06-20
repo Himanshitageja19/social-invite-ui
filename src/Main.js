af8077209222c0c52b5ba0a588d79b6adf8d2908
Himanshitageja19 <Himanshitageja19@gmail.com>
2023-06-20 16:48:05 +0530

    Added Error Handling.

--- src/Main.js
+++ src/Main.js
@@ -8,6 +8,7 @@ function Main(props) {
  const [enterTime,setEnterTime] = useState(Math.round(new Date().getTime() / 1000))
  const [sinceCount,setSinceCount] = useState(1)
  const [loadingText,setLoadingText] = useState('Load More')
+ const [getPostsStatus, setGetPostsStatus] = useState('Loading Posts...')
 
 
  useEffect(() => {
@@ -16,18 +17,26 @@ function Main(props) {
 },[])
 
 async function getPosts(time=enterTime, relayName='wss://relay.nostr.band') {
-    const {data} =await Axios.get(`https://api.postre.io/notes?page=0&size=500&start_time=1676278400&end_time=1696278400&filter=${props.filter}`)
-    console.log(data)
-     setEvents([...data.content])
+    try {   
+        const {data} =await Axios.get(`https://api.postre.io/notes?page=0&size=500&start_time=1676278400&end_time=1696278400&filter=${props.filter}`)
+        console.log(data)
+         setEvents([...data.content])
+    } catch(err) {
+        setGetPostsStatus('Some error occured while getting posts.Please try reloading page!')
+    }
 }
 
 async function loadMore() {
     setLoadingText('Loading...')
     await setSinceCount(Number(sinceCount)+1||1);
-    const {data} =await Axios.get(`https://api.postre.io/notes?page=${sinceCount}&size=500&start_time=1676278400&end_time=1696278400&filter=${props.filter}`)
-    console.log(data)
-     setEvents([...events,...data.content])
-     setLoadingText('Load More')
+    try {
+        const {data} =await Axios.get(`https://api.postre.io/notes?page=${sinceCount}&size=500&start_time=1676278400&end_time=1696278400&filter=${props.filter}`)
+        console.log(data)
+        setEvents([...events,...data.content])
+        setLoadingText('Load More')
+    } catch(err) {
+        setLoadingText('Error! Click Again')
+    }
 }
 
 return (
@@ -49,7 +58,7 @@ return (
             :'')
         }) }
     </div> 
-     : <div className=' flex items-center justify-center  mt-24'><p className='p-0 m-0 w-36 bg-blue-500 rounded text-white'>Loading Posts...</p></div>  }
+     : <div className=' flex items-center justify-center  mt-24'><p className={getPostsStatus=== 'Loading Posts...'? 'p-0 m-0 w-36 bg-blue-500 rounded text-white':'p-2 m-0 w-128 border-solid border-gray-200 border-2 bg-white rounded text-red-400'}>{getPostsStatus}</p></div>  }
     {events.length>499?<button className="m-2 text-center text-white bg-blue-500 rounded p-2" onClick={loadMore}>{loadingText}</button>:''}
     </div>
 )
