a7bcc586201ecf6d9efdf6df835e88af0490fa24
Himanshitageja19 <Himanshitageja19@gmail.com>
2023-06-20 16:24:06 +0530

    changed the size in url.

--- src/Main.js
+++ src/Main.js
@@ -6,7 +6,7 @@ import Axios from 'axios'
 function Main(props) {
  const [events,setEvents] = useState([]);
  const [enterTime,setEnterTime] = useState(Math.round(new Date().getTime() / 1000))
- const [sinceCount,setSinceCount] = useState(0)
+ const [sinceCount,setSinceCount] = useState(1)
  const [loadingText,setLoadingText] = useState('Load More')
 
 
@@ -16,7 +16,7 @@ function Main(props) {
 },[])
 
 async function getPosts(time=enterTime, relayName='wss://relay.nostr.band') {
-    const {data} =await Axios.get(`https://54.89.215.146/notes?page=0&size=20&start_time=1676278400&end_time=1696278400&filter=${props.filter}`)
+    const {data} =await Axios.get(`https://api.postre.io/notes?page=0&size=500&start_time=1676278400&end_time=1696278400&filter=${props.filter}`)
     console.log(data)
      setEvents([...data.content])
 }
@@ -24,7 +24,7 @@ async function getPosts(time=enterTime, relayName='wss://relay.nostr.band') {
 async function loadMore() {
     setLoadingText('Loading...')
     await setSinceCount(Number(sinceCount)+1||1);
-    const {data} =await Axios.get(`https://54.89.215.146/notes?page=${sinceCount}&size=20&start_time=1676278400&end_time=1696278400&filter=${props.filter}`)
+    const {data} =await Axios.get(`https://api.postre.io/notes?page=${sinceCount}&size=500&start_time=1676278400&end_time=1696278400&filter=${props.filter}`)
     console.log(data)
      setEvents([...events,...data.content])
      setLoadingText('Load More')
@@ -50,7 +50,7 @@ return (
         }) }
     </div> 
      : <div className=' flex items-center justify-center  mt-24'><p className='p-0 m-0 w-36 bg-blue-500 rounded text-white'>Loading Posts...</p></div>  }
-    {events.length>10?<button className="m-2 text-center text-white bg-blue-500 rounded p-2" onClick={loadMore}>{loadingText}</button>:''}
+    {events.length>499?<button className="m-2 text-center text-white bg-blue-500 rounded p-2" onClick={loadMore}>{loadingText}</button>:''}
     </div>
 )
 
