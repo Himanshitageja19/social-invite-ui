a40b0b675ce1ebe3d38016e3b9fba775037c41f3
Himanshitageja19 <Himanshitageja19@gmail.com>
2023-06-28 00:35:26 +0530

    changed the size and make start time and end time dynamic.

--- src/Main.js
+++ src/Main.js
@@ -5,20 +5,38 @@ import React, { useState, useEffect } from "react";
 import Axios from 'axios'
 function Main(props) {
  const [events,setEvents] = useState([]);
- const [enterTime,setEnterTime] = useState(Math.round(new Date().getTime() / 1000))
+ const [enterTime,setEnterTime] = useState(getStartTime())
+ const [endTime,setEndTime] = useState(getEndTime())
  const [sinceCount,setSinceCount] = useState(1)
+ const [size,setSize] = useState(100)
  const [loadingText,setLoadingText] = useState('Load More')
  const [getPostsStatus, setGetPostsStatus] = useState('Loading Posts...')
 
 
  useEffect(() => {
-    setEnterTime(Math.round(new Date().getTime() / 1000));
+    setEnterTime(getStartTime())
+    setEndTime(getEndTime())
     getPosts()
 },[])
 
+function getStartTime() {
+    var currentDate = new Date();
+    currentDate.setHours(0, 0, 0, 0);
+    var epochTime = currentDate.getTime();
+    return epochTime/1000;
+}
+
+function getEndTime() {
+    var currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + 1);
+    currentDate.setHours(23, 59, 59, 0);
+    var epochTime = currentDate.getTime();
+    return epochTime/1000;
+}
+
 async function getPosts(time=enterTime, relayName='wss://relay.nostr.band') {
     try {   
-        const {data} =await Axios.get(`https://api.postre.io/notes?page=0&size=500&start_time=1676278400&end_time=1696278400&filter=${props.filter}`)
+        const {data} =await Axios.get(`https://api.postre.io/notes?page=0&size=${size}&start_time=${enterTime}&end_time=${endTime}&filter=${props.filter}`)
         console.log(data)
          setEvents([...data.content])
     } catch(err) {
@@ -30,7 +48,7 @@ async function loadMore() {
     setLoadingText('Loading...')
     await setSinceCount(Number(sinceCount)+1||1);
     try {
-        const {data} =await Axios.get(`https://api.postre.io/notes?page=${sinceCount}&size=500&start_time=1676278400&end_time=1696278400&filter=${props.filter}`)
+        const {data} =await Axios.get(`https://api.postre.io/notes?page=${sinceCount}&size=${size}&start_time=${enterTime}&end_time=${endTime}&filter=${props.filter}`)
         console.log(data)
         setEvents([...events,...data.content])
         setLoadingText('Load More')
@@ -59,7 +77,7 @@ return (
         }) }
     </div> 
      : <div className=' flex items-center justify-center  mt-24'><p className={getPostsStatus=== 'Loading Posts...'? 'p-0 m-0 w-36 bg-blue-500 rounded text-white':'p-2 m-0 w-128 border-solid border-gray-200 border-2 bg-white rounded text-red-400'}>{getPostsStatus}</p></div>  }
-    {events.length>499?<button className="m-2 text-center text-white bg-blue-500 rounded p-2" onClick={loadMore}>{loadingText}</button>:''}
+    {events.length>size-1?<button className="m-2 text-center text-white bg-blue-500 rounded p-2" onClick={loadMore}>{loadingText}</button>:''}
     </div>
 )
 
